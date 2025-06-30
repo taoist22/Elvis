9fd406ba2eae9b0b81a22d2d4a271ef16fdde7a0
CT Reatherford <taoist22@gmail.com>
2025-06-29 15:17:05 -1000

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by CT Reatherford on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
